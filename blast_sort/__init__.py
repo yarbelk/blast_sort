b0aaac73e176af8089219d5b0bacf28cefd7e8c1
yarbelk <james.rivettcarnac@gmail.com>
2013-03-16 17:51:38 +0800

    got the blast_sort cli working

--- blast_sort/__init__.py
+++ blast_sort/__init__.py
@@ -1,2 +1,3 @@
 from . parse import read_file
 from . check_inclusion import check_inclusion
+from . main import main
--- blast_sort/__main__.py
+++ blast_sort/__main__.py
@@ -1,31 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import argparse
 import sys
-from path import path
+import main
 
-from parse import read_file
-from check_inclusion import check_inclusion
-
-def main(argv):
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--blast_file', '-b')
-    arg_parser.add_argument('--data_files', '-d', nargs='+')
-    arg_parser.add_argument('--output_folder', '-o')
-    if len(argv) == 1:
-        argv.append('--help')
-    args = arg_parser.parse_args(sys.argv[1:])
-    output_folder = path(args.output_folder)
-    # check that the output folder exists
-    if not output_folder.exists():
-        output_folder.mkdir()
-    if not output_folder.isdir():
-        print "{0} already exists and is not a directory.  please choose a directory or a new name"
-        sys.exit(status=1)
-    blast_set = read_file(args.blast_file)
-    for data_file in args.data_files:
-        check_inclusion(blast_set, data_file, output_folder)
-    print "\n\nDONE! :)"
-
-main(sys.argv)
+main.main(sys.argv)
