dce2a2ac639e81e329134d8af09c214c3e0ac372
yarbelk <james.rivettcarnac@gmail.com>
2013-03-18 18:39:53 +0800

    changed to setuputils

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 #encoding: utf-8
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="blast_sort",
-        version='0.1',
+        version='0.1.1',
         description='a utility to sort blasts of various species based on a lookup table',
         author='James Rivett-Carnac',
         author_email='james.rivettcarnac@gmail.com',
         packages=['blast_sort',],
-        requires=['path.py',],
+        install_requires=['path.py',],
         scripts=['scripts/blast_sort',],
         )
