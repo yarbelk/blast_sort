87b6cee82ef2f0277a6b935cbaf880266b707b53
yarbelk <james.rivettcarnac@gmail.com>
2013-04-06 18:21:07 +0800

    incremented the version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name="blast_sort",
-        version='0.1.2',
+        version='0.1.3',
         description='a utility to sort blasts of various species based on a lookup table',
         author='James Rivett-Carnac',
         author_email='james.rivettcarnac@gmail.com',
