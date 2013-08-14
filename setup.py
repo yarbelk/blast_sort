acd3a773cb2af64a9ac4489adeb223e52207976e
yarbelk <james.rivettcarnac@gmail.com>
2013-08-14 15:07:39 +0800

    rename project to query_contig_sort and change setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 
 from setuptools import setup
 
-setup(name="blast_sort",
-        version='0.1.3',
-        description='a utility to sort blasts of various species based on a lookup table',
+setup(name="query_contig_sort",
+        version='0.1.4',
+        description='a utility to sort blasts of various species for local blast with the fasta names all begining with Query_contig_',
         author='James Rivett-Carnac',
         author_email='james.rivettcarnac@gmail.com',
-        packages=['blast_sort',],
+        packages=['query_contig_sort',],
         install_requires=['path.py',],
-        scripts=['scripts/blast_sort',],
+        scripts=['scripts/query_contig_sort',],
         test_suite= 'nose.collector',
         test_requires = ['nose',]
         )
