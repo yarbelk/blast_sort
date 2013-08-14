3428a04574b1bb74f5d2ad385faad2b01225e525
yarbelk <james.rivettcarnac@gmail.com>
2013-08-14 15:04:29 +0800

    Removed the blast_set lookup.  changed the line checks to only look if the line beginds with Query_contig_, rather than the complicated lookup

--- blast_sort/check_inclusion.py
+++ blast_sort/check_inclusion.py
@@ -11,47 +11,8 @@ Identity = namedtuple('Identity', ['name','dna'])
 # create unbuffered stdout so updates print nicely
 
 
-re_identity = re.compile(r"^[\w_\d]+$")
-re_blast_no = re.compile(r"^gi\|\d+\|\w+\|(?P<blast>\w{2,3}_?\d+)\.?\d*\|")
+re_query_contig = re.compile(r"^Query_contig_\d+$")
 
-re_gi = re.compile(r"^gi\|\d+\|(?P<type>\w+)")
-re_refseq = re.compile(r"ref\|(?P<accession>[\w\d_]+)(\.\d+)?\|(?P<name>[\w\d_]*)")
-re_genbank = re.compile(r"gb\|(?P<accession>[\w\d_]+)(\.\d+)?\|(?P<locus>[\w\d]*)")
-re_embl = re.compile(r"emb\|(?P<accession>[\w\d_]+)(\.\d+)?\|(?P<locus>[\w\d]*)")
-re_pir = re.compile(r"pir\|(?P<accession>[\w\d_]*)\|(?P<name>[\w\d_]*)")  # Doesn't seem to have accession numbers
-re_ddbj = re.compile(r"djb\|(?P<accession>[\w\d_]*)(\.\d+)?\|(?P<locus>[\w\d_]*)")
-re_prf = re.compile(r"prf\|(?P<accession>[\w\d_]*)(\.\d+)?\|(?P<locus>[\w\d_]*)")  # Doesn't seem to have accession numbers
-
-re_dict = {
-    'ref': re_refseq,
-    'gb' : re_genbank,
-    'emb': re_embl,
-    'pir': re_pir,
-    'djb': re_ddbj,
-    'prf': re_prf,
-    }
-
-def get_blast_dict(data):
-    gi_search = re_gi.search(data)
-    if gi_search:
-        try:
-            blast_type = gi_search.groupdict()['type']
-            test_re = re_dict[blast_type]
-            data_search = test_re.search(data)
-            if data_search:
-                found = data_search.groupdict()
-                blast = found['accession'] if found['accession'] else found['name']
-                ret_data = {
-                        'blast_type' : blast_type,
-                        'blast'      : blast.split('.')[0],
-                        'name'       : found.get('name'),
-                        'locus'      : found.get('locus'),
-                        }
-                return ret_data
-            return None
-        except KeyError:
-            return None
-    return None
 
 def format_fasta(identity):
     sequence = identity.dna
@@ -61,7 +22,7 @@ def format_fasta(identity):
         sequence = sequence[79:]
     return split_sequence
 
-def check_inclusion(blast_set, data_file, output_folder, verbose=False):
+def check_inclusion(data_file, output_folder, verbose=False):
     output_folder = path(output_folder)
     identity = None
     identity_old = None
@@ -79,8 +40,10 @@ def check_inclusion(blast_set, data_file, output_folder, verbose=False):
                 if verbose:
                     print "{0}%".format(cur_percent)
             last_percent = cur_percent
-            id_match = re_identity.search(line[0])
-            if id_match:
+
+            # Is this a new Identity?
+            blast_match = re_query_contig.search(line[0])
+            if not blast_match:
                 if data_column is None:
                     if len(line) > 2 and line[2]:
                         data_column = 2
@@ -89,16 +52,13 @@ def check_inclusion(blast_set, data_file, output_folder, verbose=False):
                 identity_old = identity
                 identity = Identity(line[0], line[data_column])
             else:
-                blast_match = get_blast_dict(line[0])
-                if blast_match:
-                    blast_no = blast_match['blast']
-                    if blast_no.split('.',1)[0] in blast_set:
-                        if blast_no != blast_no_old and identity.name != getattr(identity_old, 'name', None):
-                            # write line(s) to file
-                            with open(output_folder / blast_no + '.fasta','a') as output_fd:
-                                output_fd.write('\n'.join(format_fasta(identity)))
-                                output_fd.flush()
-                        blast_no_old = blast_no
+                query_name = line[0]
+                if query_name != query_name_old and identity.name != getattr(identity_old, 'name', None):
+                    # write line(s) to file
+                    with open(output_folder / blast_no + '.fasta','a') as output_fd:
+                        output_fd.write('\n'.join(format_fasta(identity)))
+                        output_fd.flush()
+                query_name_old = query_name
 
 
 if __name__ == '__main__':
--- blast_sort/main.py
+++ blast_sort/main.py
@@ -8,9 +8,6 @@ from check_inclusion import check_inclusion
 def main(argv):
     arg_parser = argparse.ArgumentParser(prog="blast_sort",
             description="A program to look up blast numbers and associated DNA/Protein data from a data file, check it against a list of blasts you want to compare, and save the output of all of the finds in fasta formated files named after the blast number")
-    arg_parser.add_argument('--blast_file', '-b',
-            required=True,
-            help="csv lookup for blast nos")
     arg_parser.add_argument('--data_files', '-d', nargs='+',
             required=True,
             help="data files to search")
@@ -31,9 +28,8 @@ def main(argv):
     if not output_folder.isdir():
         print "{0} already exists and is not a directory.  please choose a directory or a new name"
         sys.exit(status=1)
-    blast_set = read_file(args.blast_file)
     for data_file in args.data_files:
-        check_inclusion(blast_set, data_file, output_folder, args.verbose)
+        check_inclusion(data_file, output_folder, args.verbose)
     if args.verbose:
         print "\n\nDONE! :)"
 
